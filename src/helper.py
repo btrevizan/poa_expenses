cf8138f4655cf725b1eacb143d6e0323f59bffb4
Bernardo Trevizan <trevizanbernardo@gmail.com>
2017-12-27 22:08:51 -0200

    Delete uneeded functions

--- src/helper.py
+++ src/helper.py
@@ -1,47 +1,5 @@
 from time import mktime
-from pandas import read_csv
 from datetime import datetime
-
-
-def read(filepath, fields, sep=';', decimal=','):
-    """Read, select columns and clear data.
-
-    Keyword arguments:
-        filepath -- path to file
-        fields -- list of column indexes
-        sep -- separator (default ';')
-        decimal -- decimal separatore (default ',')
-    """
-    data = read_csv(filepath, sep=sep, decimal=decimal)
-    data = data.iloc[:, fields]
-
-    return data
-
-
-def clear(data):
-    """Clear string and format dates.
-
-    Keyword argument:
-        data -- a DataFrame
-    """
-    n, m = data.shape
-
-    # For each type of data (columns has same type in every line)
-    for j in range(m):
-        sample = data.iloc[0, j]
-
-        if len(str(sample).split('-')) == 3 and 'T00:00:00' in sample:
-            dtype = to_date
-        elif type(sample) is str:
-            dtype = to_str
-        else:
-            dtype = lambda x: x
-
-        # For each line in column j
-        for i in range(n):
-            data.iloc[i, j] = dtype(data.iloc[i, j])
-
-    return data
 
 
 def to_date(value):
@@ -51,7 +9,7 @@ def to_date(value):
         value -- value to be converted
     """
     y, m, d = value.split('-')
-    d, _ = d.split('T')
+    d = d[:2]
 
     date = datetime.strptime('{}-{}-{}'.format(y, m, d), '%Y-%m-%d')
     timestamp = mktime(date.timetuple())
